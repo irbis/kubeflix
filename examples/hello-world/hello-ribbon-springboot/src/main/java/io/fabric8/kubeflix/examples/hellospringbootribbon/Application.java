a7f87f764d3e0013e7172b7d0a7d167a75c303dd
Oleksii Nadtochyi <oleksii_nadtocyi@epam.com>
2017-06-17 23:25:44 +0300

    Fix RestTemplate bean instantiation according to SpringBoot version of 1.4.1.RELEASE
    SpringBoot version requires to use RestTemplateBuilder to create object of RestTemplate.

--- examples/hello-world/hello-ribbon-springboot/src/main/java/io/fabric8/kubeflix/examples/hellospringbootribbon/Application.java
+++ examples/hello-world/hello-ribbon-springboot/src/main/java/io/fabric8/kubeflix/examples/hellospringbootribbon/Application.java
@@ -18,8 +18,11 @@ package io.fabric8.kubeflix.examples.hellospringbootribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableCircuitBreaker
@@ -28,5 +31,10 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 }
